1245598962d6596050ef0c1437fdf323eaac0659
Ryan Kooy <rwkooy@gmail.com>
2019-08-10 12:22:31 -0400

    3 database js files

---
+++ assets/javascript/appChatBox.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyDGxeGNaPIi0Vp018GM7Br7SYonsYESqhg",
+   authDomain: "project-1-4a8dc.firebaseapp.com",
+   databaseURL: "https://project-1-4a8dc.firebaseio.com",
+   projectId: "project-1-4a8dc",
+   storageBucket: "",
+   messagingSenderId: "650110209456",
+   appId: "1:650110209456:web:ab026fe2e1fdc005"
+};
+
+firebase.initializeApp(firebaseConfig);
