75a99a3dc1a8019deb29237fa8bc788925ba496a
Ethan <allric0922@gmail.com>
2019-08-13 16:27:47 -0400

    majority of triva text in complete. all using 'Test' classes/ids, those need updated.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,15 +14,39 @@ $.ajax({
     method: 'GET'
 }).then(function(response){
     res = response.results
-    for(var i = 0; i < res.length; i++){
-        console.log(i, res[i].question)
-        
-    }
+    console.log(res)
     $(".TTTboard").show()
 })
 })
-//-----------------------------------------------------------------------------------------------------
+
+var correctAns = 0
+
 $(document).on("click", ".TTTboard", function(){
     var x = $(this).val()
-    console.log(res[x].question)
+    var correctAns = Math.floor(Math.random() * (4 - 0))
+    var answers = res[x].incorrect_answers
+    answers.splice(correctAns, 0 , res[x].correct_answer)
+
+    for(var i = 0; i < answers.length; i++){
+        var answerButton = $("<button>")
+        answerButton.attr("value", i)
+        answerButton.attr("class", "guess")
+        answerButton.text(answers[i])
+        $(".answersTest").append(answerButton)
+        console.log(answerButton)
+    }
+
+    var questionH1 = $("<h1>")
+    questionH1.attr("class", "question")
+    questionH1.text(res[x].question)
+    $(".questionsTest").append(questionH1)
+})
+
+$(document).on("click", ".guess", function(){
+    if ($(this).val() == correctAns){
+        console.log("correct")
+    }
+    else{
+        console.log("wrong")
+    }
 })
