bfaac66c7595d143e1a4bdeb2f1027319a886543
Ethan <allric0922@gmail.com>
2019-08-17 10:28:37 -0400

    minor changes, commited to pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,36 +32,6 @@ $.ajax({
 // filler var for finding the correct answer
 var correctAns = 0
 
-// //when a spot on the board is clicked
-// $(document).on("click", ".TTTboard", function(){
-
-//     //gets the spot on the board where the user selects and pulls the value (0-8) to get the equivilent number in the question list
-//     var x = $(this).val()
-
-//     //picks a random number 0-3 and splices the correct answer into the API's incorrect answer array
-//     correctAns = Math.floor(Math.random() * (4 - 0))
-//     var answers = res[x].incorrect_answers
-//     answers.splice(correctAns, 0 , res[x].correct_answer)
-//     // console.log (correctAns)
-
-//     //for loop to create 4 buttons with the answer array in them
-//     for(var i = 0; i < answers.length; i++){
-//         var answerButton = $("<button>")
-//         answerButton.attr("value", i)
-//         answerButton.attr("class", "guess")
-//         answerButton.text(answers[i])
-//         $(".answersTest").append(answerButton)
-//     }
-
-//     // adds the question to the page based on which board button was clicked. 
-//     var questionH1 = $("<h1>")
-//     questionH1.attr("class", "question")
-//     questionH1.text(res[x].question)
-//     $(".questionsTest").append(questionH1)
-
-
-// })
-
 //when the answer is clicked checks to see if the answer is correct.
 $(document).on("click", ".guess", function(){
     if ($(this).val() == correctAns){
@@ -72,6 +42,7 @@ $(document).on("click", ".guess", function(){
     else{
         console.log("wrong")
         clearScrn()
+        $(".TTTboard").show()
     }
 })
 
